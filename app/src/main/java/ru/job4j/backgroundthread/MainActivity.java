3365e093e225e8192cc6128adcb831302fd5ed10
AlekseevArtem <tema9991@mail.ru>
2020-07-20 13:18:58 +0300

    01 - Thread, Runnable [#294972]

--- app/src/main/java/ru/job4j/backgroundthread/MainActivity.java
+++ app/src/main/java/ru/job4j/backgroundthread/MainActivity.java
@@ -3,12 +3,27 @@ package ru.job4j.backgroundthread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TestRunnable runnable;
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startThread(View view){
+        runnable = new TestRunnable(10);
+    }
+
+    public void stopThread(View view){
+        runnable.stopThread();
+        Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
     }
 }
