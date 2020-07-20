b4d91e5d0ad5347a21f6f5e969ea2028c8ee1711
AlekseevArtem <tema9991@mail.ru>
2020-07-20 15:51:11 +0300

    02 - Main (UI) Thread, runOnUiThread [#294973]

--- app/src/main/java/ru/job4j/backgroundthread/MainActivity.java
+++ app/src/main/java/ru/job4j/backgroundthread/MainActivity.java
@@ -2,13 +2,24 @@ package ru.job4j.backgroundthread;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-    private TestRunnable runnable;
+    private TestRunnable mStartTest;
+    private ImageRunnable mLoadImage;
+    private TextView mText;
+    private ImageView mImage;
 
     public static final String TAG = "MainActivity";
 
@@ -16,14 +27,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mText = findViewById(R.id.text);
+        mImage = findViewById(R.id.image);
     }
 
     public void startThread(View view){
-        runnable = new TestRunnable(10);
+        mStartTest = new TestRunnable(10);
     }
 
     public void stopThread(View view){
-        runnable.stopThread();
+        mStartTest.stopThread();
         Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadImage(View view){
+        mLoadImage = new ImageRunnable("https://2ch.hk/b/arch/2020-05-03/src/219280082/15884658224810.png");
+    }
+
+    class TestRunnable implements Runnable {
+        private volatile boolean stopFlag = false;
+        Thread t;
+        private int times;
+
+        public TestRunnable(int times) {
+            this.times = times;
+            t = new Thread(this);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            int count = 0;
+            while (count != times && !stopFlag) {
+                String percent = count * 10 + "%";
+                runOnUiThread(() -> mText.setText(percent));
+                Log.d(TAG, "startThread: " + count);
+                count++;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        synchronized void stopThread() {
+            stopFlag = true;
+        }
+    }
+
+    class ImageRunnable implements Runnable {
+        Thread t;
+        private String url;
+
+        public ImageRunnable(String url) {
+            this.url = url;
+            t = new Thread(this);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            Bitmap bitmap = loadImageFromNetwork(url);
+            runOnUiThread(() -> mImage.setImageBitmap(bitmap));
+        }
+
+        private Bitmap loadImageFromNetwork(String url) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory
+                        .decodeStream((InputStream) new URL(url)
+                                .getContent());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
     }
 }
